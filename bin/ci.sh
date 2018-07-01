75f38816f2fc3dae577e534b14d16d7e54f71d3d
John Sirois <john.sirois@gmail.com>
2018-07-01 17:41:31 -0600

    Button up release bundling.

--- bin/ci.sh
+++ bin/ci.sh
@@ -6,6 +6,5 @@ ROOT="$(git rev-parse --show-toplevel)"
 
 # TODO(John Sirois): Conditionally generate the travis image and push or else pull it.
 
-${ROOT}/bin/dev_env.sh cargo make --cwd server generate-favicon
-${ROOT}/bin/dev_env.sh brunch build app
-${ROOT}/bin/dev_env.sh cargo make --cwd server
+${ROOT}/bin/dev_env.sh cargo make --cwd server ci-flow
+${ROOT}/bin/dev_env.sh cargo make --cwd server bundle
