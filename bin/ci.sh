161ea84ff9830a072b823f71adbf4fa34fffc55f
John Sirois <john.sirois@gmail.com>
2018-07-02 09:07:35 +0000

    Kill obsolete TODO.

--- bin/ci.sh
+++ bin/ci.sh
@@ -4,7 +4,5 @@ set -euo pipefail
 
 ROOT="$(git rev-parse --show-toplevel)"
 
-# TODO(John Sirois): Conditionally generate the travis image and push or else pull it.
-
 ${ROOT}/bin/dev_env.sh cargo make --cwd server ci-flow
 ${ROOT}/bin/dev_env.sh cargo make --cwd server bundle
