e38b24b336c91fa66fe9e6b3789340b548f06651
John Sirois <john.sirois@gmail.com>
2018-07-02 11:51:56 -0600

    Add OSX to CI.

--- bin/ci.sh
+++ bin/ci.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 ROOT="$(git rev-parse --show-toplevel)"
 
-${ROOT}/bin/dev_env.sh cargo make --cwd server ci-flow
-${ROOT}/bin/dev_env.sh cargo make --cwd server bundle
+cargo make --cwd ${ROOT}/server ci-flow
+cargo make --cwd ${ROOT}/server bundle
