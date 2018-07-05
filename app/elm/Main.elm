b2bee297ce690255770158a23612658bbee4dd4b
John Sirois <john.sirois@gmail.com>
2018-07-04 21:16:57 -0600

    Dabble in event pump.

--- app/elm/Main.elm
+++ app/elm/Main.elm
@@ -1,6 +1,41 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (Html, text)
+import Window exposing (resizes, Size)
+
+
+type alias Model =
+    Size
+
+
+init : ( Model, Cmd WindowResize )
+init =
+    ( { width = 0, height = 0 }, Cmd.none )
+
+
+view : Model -> Html WindowResize
+view model =
+    text ("Window is " ++ toString model.width ++ "x" ++ toString model.height)
+
+
+type alias WindowResize =
+    Size
+
+
+update : WindowResize -> Model -> ( Model, Cmd WindowResize )
+update resize size =
+    ( resize, Cmd.none )
+
+
+handleResize : Size -> WindowResize
+handleResize size =
+    size
+
+
+subscriptions : Model -> Sub WindowResize
+subscriptions model =
+    Window.resizes handleResize
+
 
 main =
-  text "Hello from Elm!"
+    Html.program { init = init, view = view, update = update, subscriptions = subscriptions }
